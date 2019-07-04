347b3ded4e54f624e6974b62ec821e0720f39dde
Murad <gmp1985@list.ru>
2019-07-04 14:13:51 +0300

    файл задания №2

--- shop.sql
+++ shop.sql
@@ -1,28 +1,28 @@
 DROP TABLE IF EXISTS catalogs;
 CREATE TABLE catalogs (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NULL COMMENT 'РќР°Р·РІР°РЅРёРµ СЂР°Р·РґРµР»Р°'/*,
-  UNIQUE unique_name(name(10)) РЈСЃР»РѕРІРёРµ РЅР°РґРѕ СѓР±СЂР°С‚СЊ, С‚Р°Рє РєР°Рє Р±СѓРґРµС‚ РІС‹РґР°РІР°С‚СЊ РѕС€РёР±РєСѓ РµСЃР»Рё Р·РЅР°С‡РµРЅРёР№ РЅРѕР»СЊ РёР»Рё РїСѓСЃС‚С‹С… СЃС‚СЂРѕРє Р±СѓРґРµС‚ Р±РѕР»СЊС€Рµ РѕРґРЅРѕР№ */
-) COMMENT = 'Р Р°Р·РґРµР» РёРЅС‚РµСЂРµРЅРµС‚ РјР°РіР°Р·РёРЅР°'
+  name VARCHAR(255) NULL COMMENT 'Название раздела'/*,
+  UNIQUE unique_name(name(10)) Условие надо убрать, так как будет выдавать ошибку если значений ноль или пустых строк будет больше одной*/
+) COMMENT = 'Раздел интеренет магазина'
 
 
--- INSERT INTO catalogs VALUES (NULL, 'РџСЂРѕС†РµСЃСЃРѕСЂС‹');
--- INSERT INTO catalogs (id, name) VALUES (NULL, 'РњР°С‚.РїР»Р°С‚С‹');
--- INSERT INTO catalogs VALUES (DEFAULT, 'Р’РёРґРµРѕРєР°СЂС‚С‹');
+-- INSERT INTO catalogs VALUES (NULL, 'Процессоры');
+-- INSERT INTO catalogs (id, name) VALUES (NULL, 'Мат.платы');
+-- INSERT INTO catalogs VALUES (DEFAULT, 'Видеокарты');
 
 INSERT IGNORE INTO catalogs VALUES 
-  (DEFAULT, 'РџСЂРѕС†РµСЃСЃРѕСЂС‹'),
-  (DEFAULT, 'РњР°С‚.РїР»Р°С‚С‹'),
-  (DEFAULT, 'Р’РёРґРµРѕРєР°СЂС‚С‹'),
+  (DEFAULT, 'Процессоры'),
+  (DEFAULT, 'Мат.платы'),
+  (DEFAULT, 'Видеокарты'),
   (DEFAULT, 0),
   (DEFAULT, NULL);
   
 UPDATE
 	catalogs
 SET
-	name = 'РџСЂРѕС†РµСЃСЃРѕСЂС‹ (INTEL)'
+	name = 'Процессоры (INTEL)'
 WHERE
-	name = 'РџСЂРѕС†РµСЃСЃРѕСЂС‹';
+	name = 'Процессоры';
 
 UPDATE
 	catalogs
@@ -36,9 +36,9 @@ SELECT id, name FROM catalogs;
 DROP TABLE IF EXISTS cat;
 CREATE TABLE cat (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NULL COMMENT 'РќР°Р·РІР°РЅРёРµ СЂР°Р·РґРµР»Р°'/*,
+  name VARCHAR(255) NULL COMMENT 'Название раздела'/*,
   UNIQUE unique_name(name(10))*/
-) COMMENT = 'Р Р°Р·РґРµР» РёРЅС‚РµСЂРµРЅРµС‚ РјР°РіР°Р·РёРЅР°'
+) COMMENT = 'Раздел интеренет магазина'
 
 INSERT INTO
 	cat 
@@ -52,11 +52,11 @@ SELECT * FROM cat;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'РРјСЏ РїРѕРєСѓРїР°С‚РµР»СЏ',
-  birthday_at DATE  COMMENT 'Р”Р°С‚Р° СЂРѕР¶РґРµРЅРёСЏ',
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE  COMMENT 'Дата рождения',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'РџРѕРєСѓРїР°С‚РµР»Рё'
+) COMMENT = 'Покупатели'
 
 INSERT INTO users (id, name, birthday_at) VALUES ( 1, 'Hello', '1970-01-27');
 SELECT * FROM users;
@@ -64,14 +64,14 @@ SELECT * FROM users;
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'РќР°Р·РІР°РЅРёРµ',
-  description TEXT COMMENT 'РћРїРёСЃР°РЅРёРµ',
-  price DECIMAL(11,2) COMMENT 'Р¦РµРЅР°',
+  name VARCHAR(255) COMMENT 'Название',
+  description TEXT COMMENT 'Описание',
+  price DECIMAL(11,2) COMMENT 'Цена',
   catalog_id INT UNSIGNED,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   KEY index_of_catalog_id(catalog_id)
-) COMMENT = 'Р Р°Р·РґРµР» РёРЅС‚РµСЂРµРЅРµС‚ РјР°РіР°Р·РёРЅР°'
+) COMMENT = 'Раздел интеренет магазина'
 
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders (
@@ -80,49 +80,47 @@ CREATE TABLE orders (
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   KEY index_of_user_id(user_id)
-) COMMENT = 'Р—Р°РєР°Р·С‹'
+) COMMENT = 'Заказы'
 
 DROP TABLE IF EXISTS orders_products;
 CREATE TABLE orders_products (
   id SERIAL PRIMARY KEY,
   order_id INT UNSIGNED,
   product_id INT UNSIGNED,
-  total INT UNSIGNED DEFAULT 1 COMMENT 'РљРѕР»РёС‡РµСЃС‚РІРѕ Р·Р°РєР°Р·Р°РЅРЅС‹С… С‚РѕРІР°СЂРЅС‹С… РїРѕР·РёС†РёР№',
+  total INT UNSIGNED DEFAULT 1 COMMENT 'Количество заказанных товарных позиций',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP  
-) COMMENT = 'РЎРѕСЃС‚Р°РІ Р·Р°РєР°Р·Р°'
+) COMMENT = 'Состав заказа'
 
 DROP TABLE IF EXISTS discounts;
 CREATE TABLE discounts (
   id SERIAL PRIMARY KEY,
   user_id INT UNSIGNED,
   product_id INT UNSIGNED,
-  discount FLOAT UNSIGNED COMMENT 'Р’РµР»РёС‡РёРЅР° СЃРєРёРґРєРё РѕС‚ 0.0 РґРѕ 1.0',
+  discount FLOAT UNSIGNED COMMENT 'Величина скидки от 0.0 до 1.0',
   started_at DATETIME,
   finish_at DATETIME,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   KEY index_of_user_id(user_id),
   KEY index_of_product_id(product_id)
-) COMMENT = 'РЎРєРёРґРєРё'
+) COMMENT = 'Скидки'
 
 
 DROP TABLE IF EXISTS storehauses;
 CREATE TABLE storehauses (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) COMMENT 'РќР°Р·РІР°РЅРёРµ',
+  name VARCHAR(255) COMMENT 'Название',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'CРєР»Р°РґС‹'
+) COMMENT = 'Cклады'
 
 DROP TABLE IF EXISTS storehauses_product;
 CREATE TABLE storehauses_product (
   id SERIAL PRIMARY KEY,
   storehause_id INT UNSIGNED,
   product_id INT UNSIGNED,
-  value INT UNSIGNED COMMENT 'Р—Р°РїР°СЃ С‚РѕРІР°СЂРЅРѕР№ РїРѕР·РёС†РёРё РЅР° СЃРєР»Р°РґРµ',
+  value INT UNSIGNED COMMENT 'Запас товарной позиции на складе',
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) COMMENT = 'Р—Р°РїР°СЃС‹ РЅР° СЃРєР»Р°РґРµ'
-
-
+) COMMENT = 'Запасы на складе'
